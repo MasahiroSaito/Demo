6fe8c426bd563662726f3abc9b8867f7e6a5cfb2
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-03-23 20:44:38 +0900

    Update EmojiController

--- src/main/kotlin/com/masahirosaito/EmojiController.kt
+++ src/main/kotlin/com/masahirosaito/EmojiController.kt
@@ -10,7 +10,18 @@ import org.springframework.web.bind.annotation.RestController
 class EmojiController {
 
     @RequestMapping(value = "/emoji", method = arrayOf(RequestMethod.POST))
-    fun index(@RequestParam param: String): String {
-        return param
+    fun index(
+            @RequestParam token: String,
+            @RequestParam team_id: String,
+            @RequestParam team_domain: String,
+            @RequestParam channel_id: String,
+            @RequestParam channel_name: String,
+            @RequestParam timestamp: String,
+            @RequestParam user_id: String,
+            @RequestParam user_name: String,
+            @RequestParam text: String,
+            @RequestParam trigger_word: String
+    ): String {
+        return token
     }
 }
