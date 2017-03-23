5cd8a281d7994a0468528d6febbb9f73329e5b5d
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-03-23 17:52:45 +0900

    Update HelloController

--- src/main/kotlin/com/masahirosaito/HelloController.kt
+++ src/main/kotlin/com/masahirosaito/HelloController.kt
@@ -1,13 +1,20 @@
 package com.masahirosaito
 
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HelloController {
 
-    @RequestMapping("/")
-    fun index(): String {
+    @RequestMapping(value = "/", method = arrayOf(RequestMethod.GET))
+    fun index(@RequestParam token: String): String {
         return "This is Spring Boot Application."
+    }
+
+    @RequestMapping(value = "/")
+    fun index(): String {
+        return "Kotlin Spring Boot Application in Heroku."
     }
 }
